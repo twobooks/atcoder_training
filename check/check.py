0accb1dfbb1a8aca68f7812367d70fe4a9a544b1
twobooks <twobookscom@gmail.com>
2020-02-23 20:59:21 +0900

    checkに追記。組合せ

--- check/check.py
+++ check/check.py
@@ -23,3 +23,25 @@ a // b        # 切り捨て除算
 # コンソールのクリア
 os.sysytem("cls")
 
+# 組合せの計算
+def comb(n:int,k:int,MOD:int):
+    """return nCk (mod MOD)
+    """
+    nCk = 1
+    # n!/(n-k)!
+    for i in range(n-k+1, n+1):
+        nCk *= i
+        nCk %= MOD
+    # 1/k!
+    for i in range(1,k+1):
+        nCk *= pow(i,MOD-2,MOD)
+        nCk %= MOD
+    return nCk
+
+# 繰返し二乗法
+# pythonだと標準関数でOK
+pow(n,p,mod) # n:底、p:指数、mod:mod
+
+
+
+
