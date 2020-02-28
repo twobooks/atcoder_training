d63901e30103cf2407869e0d677ec4b4c21d1fba
twobooks <twobookscom@gmail.com>
2020-02-29 03:44:29 +0900

    ABC151A - Next Alphabetを回答した

---
+++ ABC151/ABC151A.py
@@ -0,0 +1,15 @@
+# import math
+# from fractions import gcd
+
+# import numpy as np
+# import scipy as scp
+
+# 入力の受け取り
+c = input()
+
+strlist = "abcdefghijklmnopqrstuvwxyz"
+
+idx = strlist.find(c) +1
+ans = strlist[idx]
+
+print(ans)
