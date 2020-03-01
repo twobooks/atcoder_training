318fcf004eb0e865ad26f805b6a07b24424a3ed1
twobooks <twobookscom@gmail.com>
2020-03-01 19:32:41 +0900

    ABC149	A. Stringsを回答した

---
+++ ABC149/ABC149A.py
@@ -0,0 +1,13 @@
+# import math
+# from itertools import permutations as permus
+# from fractions import gcd
+
+# import numpy as np
+# import scipy as scp
+
+# 入力の受け取り
+s,t = input().split()
+
+ans = t + s
+
+print(ans)
