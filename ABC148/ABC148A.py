3334d8ca9bf6c192f2dc93fafc8bf6adb09f7cfe
twobooks <twobookscom@gmail.com>
2020-03-03 22:02:34 +0900

    ABC148A - Round Oneを回答した

---
+++ ABC148/ABC148A.py
@@ -0,0 +1,14 @@
+# import math
+# from itertools import permutations as permus
+# from fractions import gcd
+
+# import numpy as np
+# import scipy as scp
+
+# 入力の受け取り
+a = int(input())
+b = int(input())
+ 
+ans = 6-a-b
+
+print(ans)
